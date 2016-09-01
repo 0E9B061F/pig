86ad14bfd18a56dacf984840ecf7c4f1f7e35c51
M. Sharpless <macquarie.sharpless@gmail.com>
2016-09-01 08:57:29 -0400

    Handle redirects and filename slugification

--- imgscraper.py
+++ imgscraper.py
@@ -12,6 +12,7 @@ import requests
 
 # need something to compare image lickness, invariant of image scale
 # calculate speed at end; total downloaded / run time
+# record total requests made, number of redirects, failures, and total images downloaded
 
 def msg():
     pass
@@ -33,57 +34,91 @@ class ImageScraper:
     downloaded = []
     retry_limit = 3
     retry_count = 0
+    redirect_limit = 2
 
     def __init__(self, root_url, downloads_path=False):
         self.root = root_url
         self.slug = slugify(self.root)
         if downloads_path:
-            self.downloads = downloads_path
+            self.downloads = os.path.join(downloads_path, self.slug)
         else:
             self.downloads = os.path.join(os.getcwd(), self.slug)
-        os.makedirs(self.downloads)
+        try:
+            os.makedirs(self.downloads)
+        except OSError:
+            if not os.path.isdir(self.downloads):
+                raise
 
     def download_addresses(self, addresses):
         for url in addresses:
             print "processing `" + url + "`"
             url = urlparse(url)
             if not url.path.endswith(self.image_extensions):
+                print "  no image extension, skipping"
                 continue
             filename = os.path.basename(url.path)
+            if not url.scheme:
+                rootscheme = urlparse(self.root).scheme
+                if rootscheme:
+                    scheme = rootscheme
+                else:
+                    scheme = 'http'
+            else:
+                scheme = False
             if not url.netloc:
                 url = urljoin(self.root, url.path)
             else:
-                url = urlunparse(url)
+                if scheme:
+                    url = scheme + ':' + urlunparse(url)
+                else:
+                    url = urlunparse(url)
             if not url in self.downloaded:
-                print "  getting `" + url + "`"
-                print "  downloading to `" + filename + "`"
                 self.download_address(url, filename)
             else:
                 print "  already downloaded, skipping"
 
-    def download_address(self, url, filename):
-        filename = os.path.join(self.downloads, filename)
-        img = requests.get(url, stream=True)
-        if img.status_code == 200:
-            with open(filename, 'wb') as f:
-                img.raw.decode_content = True
-                shutil.copyfileobj(img.raw, f)
-            self.downloaded.append(url)
+    def resolve(self, url):
+        r = requests.head(url)
+        redirects = 0
+        while 'Location' in r.headers and redirects < self.redirect_limit:
+            r = requests.head(r.headers['Location'])
+            redirects += 1
+        if 'Location' in r.headers or not 'Content-Type' in r.headers:
+            return False
         else:
-            print "failed getting `" + url + "` with code " + img.status_code
-            self.failed.append(url)
+            return r
+
+    def download_address(self, url, filename):
+        name, ext = os.path.splitext(filename)
+        filename = slugify(name) + ext
+        filename = os.path.join(self.downloads, filename)
+        r = self.resolve(url)
+        if r and r.headers['Content-Type'].startswith('image'):
+            img = requests.get(url, stream=True)
+            if img.status_code == 200:
+                print "  getting `" + url + "`"
+                print "  downloading to `" + filename + "`"
+                with open(filename, 'wb') as f:
+                    img.raw.decode_content = True
+                    shutil.copyfileobj(img.raw, f)
+                self.downloaded.append(url)
+            else:
+                print "failed getting `" + url + "` with code " + img.status_code
+                self.failed.append(url)
+        else:
+            print "  not an image"
 
     def scrape(self):
         page = requests.get(self.root)
         tree = html.fromstring(page.content)
-        addresses = tree.xpath('//a/@href') + tree.xpath('//img/@src')
+        addresses = tree.xpath('//img/@src') + tree.xpath('//a/@href')
         self.download_addresses(addresses)
-        while failed.length > 0 and retry_count <= retry_limit:
-            retry_count += 1
-            print "Some image downloads failed; retrying, attempt no. " + retry_count
-            last_failed = failed
-            failed = []
+        while len(self.failed) > 0 and self.retry_count <= self.retry_limit:
+            self.retry_count += 1
+            print "Some image downloads failed; retrying, attempt no. " + self.retry_count
+            last_failed = self.failed
+            self.failed = []
             self.download_addresses(last_failed)
 
-scraper = ImageScraper('https://en.wikipedia.org/wiki/Capitoline_Brutus')
+scraper = ImageScraper('http://jbeckwith.com/2012/11/28/5-steps-to-a-better-windows-command-line/', 'test-scrapes')
 scraper.scrape()
